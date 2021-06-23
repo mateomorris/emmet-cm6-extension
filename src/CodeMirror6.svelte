6802d41e4c574ed6e56079b2950ce6eacb03a86b
mateomorris <accounts@mateomorris.com>
2021-06-23 10:15:09 -0400

    remove csspeak

--- src/CodeMirror6.svelte
+++ src/CodeMirror6.svelte
@@ -1,27 +1,26 @@
 <script>
-  import {onMount} from 'svelte'
-  import {defaultTabBinding} from "@codemirror/commands"
-  import {EditorView,keymap} from "@codemirror/view"
-  import {EditorState,Compartment} from "@codemirror/state"
-  import {basicSetup} from "@codemirror/basic-setup"
-  import {css} from "@codemirror/lang-css"
+  import { onMount } from 'svelte';
+  import { defaultTabBinding } from '@codemirror/commands';
+  import { EditorView, keymap } from '@codemirror/view';
+  import { EditorState, Compartment } from '@codemirror/state';
+  import { basicSetup } from '@codemirror/basic-setup';
+  import { css } from '@codemirror/lang-css';
   import { html } from '@codemirror/lang-html';
   import emmetExt from './emmet-codemirror6-ext';
-  import cssPeek from './css-peek';
 
-  let element
-  export let type
-  export let content
+  let element;
+  export let type;
+  export let content;
 
   function getLanguageOf(type) {
-    const language = new Compartment
+    const language = new Compartment();
     switch (type) {
       case 'stylesheet':
         return language.of(css());
       case 'markup':
         return language.of(html());
       default:
-        throw `Invalid type: ${type}. Expected 'stylesheet' or 'markup'`
+        throw `Invalid type: ${type}. Expected 'stylesheet' or 'markup'`;
     }
   }
   function getEmmetConfig(type) {
@@ -29,18 +28,18 @@
       case 'stylesheet':
         return {
           type: 'stylesheet',
-          syntax: 'css'
+          syntax: 'css',
         };
       case 'markup':
         return {
           type: 'markup',
-          syntax: 'html'
+          syntax: 'html',
         };
       default:
-        throw `Invalid type: ${type}. Expected 'stylesheet' or 'markup'`
+        throw `Invalid type: ${type}. Expected 'stylesheet' or 'markup'`;
     }
   }
-  let view
+  let view;
   onMount(() => {
     const state = EditorState.create({
       doc: content,
@@ -50,24 +49,15 @@
         emmetExt({
           config: getEmmetConfig(type),
         }),
-        cssPeek({
-          src: [
-            './style.css',
-          ],
-          css: '.container { margin: 1rem; }',
-        }),
-        keymap.of([
-          defaultTabBinding
-        ])
-      ]
-    })
+        keymap.of([defaultTabBinding]),
+      ],
+    });
 
-   view = new EditorView({
+    view = new EditorView({
       state,
-      parent: element
-    })
-  })
+      parent: element,
+    });
+  });
 </script>
 
-<div bind:this={element}>
-</div>
+<div bind:this={element} />
